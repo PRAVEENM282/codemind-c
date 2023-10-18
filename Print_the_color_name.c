c7175f8a0faecb6d67a8a41ec85c996828509496
PRAVEENM282 <147791750+PRAVEENM282@users.noreply.github.com>
2023-10-18 06:48:21 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if(c==86)     printf("Violet");     else if(c==73)     printf("Indigo");     else if(c==66)     printf("Blue");     else if(c==71)     printf("Green");     else if(c==89)     printf("Yellow");     else if(c==79)     printf("Orange");     else if(c==82)     printf("Red");     else     printf("-1");
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c==86)
+    printf("Violet");
+    else if(c==73)
+    printf("Indigo");
+    else if(c==66)
+    printf("Blue");
+    else if(c==71)
+    printf("Green");
+    else if(c==89)
+    printf("Yellow");
+    else if(c==79)
+    printf("Orange");
+    else if(c==82)
+    printf("Red");
+    else
+    printf("-1");
+    
+}
