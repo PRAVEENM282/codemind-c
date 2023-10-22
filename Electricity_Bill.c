50dde8d2279e197a1ead108d15e87c57199571cc
PRAVEENM282 <147791750+PRAVEENM282@users.noreply.github.com>
2023-10-22 07:47:07 +0530

    #include<stdio.h> int main() {     int u;     float t,c,p,cpu;     scanf("%d",&u);     if(u<200)     {         cpu=1.20;         p=0.00;         c=u*cpu;         t=c+p;     }     else if(u>=200 && u<400)     {         cpu=1.40;         p=0.00;         c=u*cpu;         t=c+p;     }     else if(u>=400 && u<600)     {         cpu=1.60;         c=u*cpu;         p=(c*15)/100.0;         t=c+p;     }        else if(u>=600 && u<800)     {         cpu=1.80;         c=u*cpu;         p=(c*15)/100.0;         t=c+p;     }        else if(u>=800)     {         cpu=2.00;         c=u*cpu;         p=(c*15)/100.0;         t=c+p;     }     printf("Units Consumed: %d ",u);     printf("Cost per Unit: %.2f ",cpu);     printf("Bill: %.2f ",c);     printf("Surcharge: %.2f ",p);     printf("Total Amount: %.2f",t);
    }

---
+++ Electricity_Bill.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+    int u;
+    float t,c,p,cpu;
+    scanf("%d",&u);
+    if(u<200)
+    {
+        cpu=1.20;
+        p=0.00;
+        c=u*cpu;
+        t=c+p;
+    }
+    else if(u>=200 && u<400)
+    {
+        cpu=1.40;
+        p=0.00;
+        c=u*cpu;
+        t=c+p;
+    }
+    else if(u>=400 && u<600)
+    {
+        cpu=1.60;
+        c=u*cpu;
+        p=(c*15)/100.0;
+        t=c+p;
+    }
+       else if(u>=600 && u<800)
+    {
+        cpu=1.80;
+        c=u*cpu;
+        p=(c*15)/100.0;
+        t=c+p;
+    }
+       else if(u>=800)
+    {
+        cpu=2.00;
+        c=u*cpu;
+        p=(c*15)/100.0;
+        t=c+p;
+    }
+    printf("Units Consumed: %d
+",u);
+    printf("Cost per Unit: %.2f
+",cpu);
+    printf("Bill: %.2f
+",c);
+    printf("Surcharge: %.2f
+",p);
+    printf("Total Amount: %.2f",t);
+    
+    
+}
