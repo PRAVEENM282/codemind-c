9fc4113c77afeb4b5535ca26828f25a17fbb4ad9
PRAVEENM282 <147791750+PRAVEENM282@users.noreply.github.com>
2023-11-10 10:58:02 +0530

    #include<stdio.h> int main(){     int i,a,b,f,s=0,k=0;     scanf("%d%d",&a,&b);     for(i=1;i<a;i++){         if(a%i==0){             s=s+i;         }     }     for(i=1;i<b;i++){         if(b%i==0){             k=k+i;         }     }         if(s==b && k==a){             printf("Amicable");         }         else {             printf("Not Amicable");         }
    }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int i,a,b,f,s=0,k=0;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<a;i++){
+        if(a%i==0){
+            s=s+i;
+        }
+    }
+    for(i=1;i<b;i++){
+        if(b%i==0){
+            k=k+i;
+        }
+    }
+        if(s==b && k==a){
+            printf("Amicable");
+        }
+        else {
+            printf("Not Amicable");
+        }
+    
+    
+    
+    
+    
+    
+}
