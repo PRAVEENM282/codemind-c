c9836539c4b2b74e5b9f2fe71f26224a8dfbacd1
PRAVEENM282 <147791750+PRAVEENM282@users.noreply.github.com>
2023-12-08 13:39:26 +0530

    #include<stdio.h> int main(){     int n,j,i;     scanf("%d",&n);     for(i=1;i<=n;i++){         for(j=i;j<=n;j++){             printf("%d ",i);
            }
            printf("
    ");
        }
    }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,j,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=i;j<=n;j++){
+            printf("%d ",i);
+            
+        }
+        printf("
+");
+    }
+}
